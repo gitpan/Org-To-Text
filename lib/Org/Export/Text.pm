085d317ae5fd6cf20f47218379ee398b5d1dd207
Steven Haryanto <stevenharyanto@gmail.com>
2014-08-16 02:21:00 +0000

    Import of SHARYANTO/Org-To-Text-0.001 from CPAN.
    gitpan-cpan-distribution: Org-To-Text
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         SHARYANTO/Org-To-Text-0.001.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/Org/Export/Text.pm
@@ -0,0 +1,51 @@
+package Org::Export::Text;
+
+use 5.010001;
+
+our $VERSION = '0.001'; # VERSION
+
+1;
+# ABSTRACT: Export Org document to text
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Org::Export::Text - Export Org document to text
+
+=head1 VERSION
+
+This document describes version 0.001 of Org::Export::Text (from Perl distribution Org-To-Text), released on 2014-08-16.
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/Org-To-Text>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/sharyanto/perl-Org-To-Text>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=Org-To-Text>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
--- lib/Org/To/Text.pm
+++ lib/Org/To/Text.pm
@@ -1,18 +1,27 @@
 package Org::To::Text;
 
-use 5.010;
+our $DATE = '2014-08-16'; # DATE
+our $VERSION = '0.001'; # VERSION
+
+use 5.010001;
 use Log::Any '$log';
 
-use vars qw($VERSION);
-
-our $VERSION = '0.00'; # VERSION
+our %SPEC;
+$SPEC{org_to_text} = {
+    v => 1.1,
+};
+sub org_to_text {
+    [502, "Not yet implemented"];
+}
 
 1;
 # ABSTRACT: Export Org document to text
 
-
 __END__
+
 =pod
+
+=encoding UTF-8
 
 =head1 NAME
 
@@ -20,7 +29,7 @@ Org::To::Text - Export Org document to text
 
 =head1 VERSION
 
-version 0.00
+This document describes version 0.001 of Org::To::Text (from Perl distribution Org-To-Text), released on 2014-08-16.
 
 =head1 SYNOPSIS
 
@@ -31,11 +40,49 @@ version 0.00
 
 NOT YET IMPLEMENTED.
 
+=head1 FUNCTIONS
+
+
+=head2 org_to_text() -> [status, msg, result, meta]
+
+No arguments.
+
+Return value:
+
+Returns an enveloped result (an array).
+
+First element (status) is an integer containing HTTP status code
+(200 means OK, 4xx caller error, 5xx function error). Second element
+(msg) is a string containing error message, or 'OK' if status is
+200. Third element (result) is optional, the actual result. Fourth
+element (meta) is called result metadata and is optional, a hash
+that contains extra information.
+
+ (any)
+
 =head1 SEE ALSO
+
+For more information about Org document format, visit http://orgmode.org/
 
 L<Org::Parser>
 
 L<Org::To::HTML>
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/Org-To-Text>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/sharyanto/perl-Org-To-Text>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=Org-To-Text>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
 
 =head1 AUTHOR
 
@@ -43,10 +90,9 @@ Steven Haryanto <stevenharyanto@gmail.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2011 by Steven Haryanto.
+This software is copyright (c) 2014 by Steven Haryanto.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
 
 =cut
-
